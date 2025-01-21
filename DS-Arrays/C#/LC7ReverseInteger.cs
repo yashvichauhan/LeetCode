a71c989d2cded425418ddb5fd5321a4e692c0910
yashvichauhan <yashvichauhan19@gmail.com>
2025-01-21 17:55:44 -0500

    reverse integer problem

---
+++ DS-Arrays/C#/LC7ReverseInteger.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int Reverse(int x) {
+                   int rev = 0;
+            int digits = 0;
+
+            while (x != 0)
+            {
+                x = Math.DivRem(x, 10, out int rem);
+                if(digits++ == 9)
+                {
+                    int max = int.MaxValue / 10;
+                    if (rev > max || (rev == max && rem > 7)) return 0;
+                    int min = int.MinValue / 10;
+                    if (rev < min || (rev == min && rem < -8)) return 0;
+                }
+
+                rev = rev * 10 + rem;
+            }
+            return rev;
+    }
+}
